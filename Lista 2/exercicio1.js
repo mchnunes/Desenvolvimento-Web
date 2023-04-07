aa60c07b18710d9a251fed777746cdf5b51ba4c9
mchnunes <84126732+mchnunes@users.noreply.github.com>
2023-04-06 22:42:20 -0300

    Entrega lista 2

---
+++ Lista 2/exercicio1.js
@@ -0,0 +1,13 @@
+var Marcos = 3200, Pedro = 3300, Michael = 4000, Neto = 5500, João = 6700
+var reajuste = (110/100)
+var valorReajuste1 = Marcos * reajuste
+var valorReajuste2 = Pedro * reajuste 
+var valorReajuste3 = Michael * reajuste
+var valorReajuste4 = Neto * reajuste
+var valorReajuste5 = João * reajuste
+
+console.log("\nMarcos Recebe R$:", Marcos, " Com reajuste de 10% passa a receber R$:", valorReajuste1.toFixed(2))
+console.log("\nPedro Recebe R$:", Pedro, "Com reajuste de 10% passa a receber R$:", valorReajuste2.toFixed(2))
+console.log("\nMichael Recebe R$:", Michael, "Com reajuste de 10% passa a receber R$:", valorReajuste3.toFixed(2))
+console.log("\nNeto Recebe R$:", Marcos, "Com reajuste de 10% passa a receber R$:", valorReajuste4.toFixed(2))
+console.log("\nJoão Recebe R$:", João,"Com reajuste de 10% passa a receber R$:", valorReajuste5.toFixed(2))
